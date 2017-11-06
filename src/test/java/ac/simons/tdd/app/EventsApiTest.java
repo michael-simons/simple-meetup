1bd5a36309bf7440100e9f0e043c760433cdbe1c
Michael J. Simons <michael@simons.ac>
2017-11-06 17:14:11 +0100

    Integrate rest-docs

--- src/test/java/ac/simons/tdd/app/EventsApiTest.java
+++ src/test/java/ac/simons/tdd/app/EventsApiTest.java
@@ -15,25 +15,98 @@
  */
 package ac.simons.tdd.app;
 
+import ac.simons.tdd.domain.Event;
 import ac.simons.tdd.domain.EventService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.hypermedia.LinksSnippet;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Michael J. Simons, 2017-11-02
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = EventsApi.class)
+@AutoConfigureRestDocs(
+      outputDir = "build/generated-api-doc",
+      uriHost = "coolevents.localhost",
+      uriPort = 80
+)
 public class EventsApiTest {
 
    @MockBean
    private EventService eventService;
 
-   @Test
-   public void registerForEvent() {
+   @Autowired
+   private MockMvc mockMvc;
 
+   private final LinksSnippet selfLink = links(linkWithRel("self").ignored().optional());
+
+   @Before
+   public void initializeMocks() {
+      final Event event1 = new Event(LocalDate.now(), "Event-1");
+      when(eventService.getOpenEvents()).thenReturn(
+            Arrays.asList(event1, new Event(LocalDate.now().plusDays(1), "Event-2"))
+      );
+      when(eventService.getEvent(event1.getHeldOn(), event1.getName())).thenReturn(Optional.of(event1));
+   }
+
+   @Test
+   public void eventsShouldWork() throws Exception {
+      this.mockMvc
+            .perform(get("/api/events").accept(HAL_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("get-events",
+                  preprocessRequest(prettyPrint()),
+                  preprocessResponse(prettyPrint()),
+                  selfLink,
+                  responseFields(
+                        subsectionWithPath("_embedded.events").description("An array of Event resources"),
+                        subsectionWithPath("_links").description("Links to other resources")
+                  )));
+
+   }
+
+   @Test
+   public void eventShouldWork() throws Exception {
+      this.mockMvc
+            .perform(get("/api/events/{heldOn}/{name}", LocalDate.now(), "Event-1").accept(HAL_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("get-event",
+                  preprocessRequest(prettyPrint()),
+                  preprocessResponse(prettyPrint()),
+                  selfLink.and(
+                        linkWithRel("registrations").description("Registrations for this event."),
+                        linkWithRel("event").description("This event.")),
+                  responseFields(
+                        fieldWithPath("heldOn").description("The date of this event."),
+                        fieldWithPath("name").description("The name of this event."),
+                        fieldWithPath("numberOfFreeSeats").description("Number of free seats left."),
+                        subsectionWithPath("_links").description("Links to other resources")
+                  )));
    }
 }
